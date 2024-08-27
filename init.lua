c39d6e72687c7e7559463b9e899b908a5debe990
Matias Aleksandrowicz <31048494+matiasalek@users.noreply.github.com>
2024-08-27 18:40:46 -0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -916,7 +916,4 @@ require('lazy').setup({
 })
 
 -- The line beneath this is called `modeline`. See `:help modeline`
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-vim.opt.expandtab = true
+-- vim: ts=2 sts=2 sw=2 et

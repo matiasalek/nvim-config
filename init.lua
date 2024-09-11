003d45b33cba15b6d265caead5a5b5c2359b9ce7
Matias Aleksandrowicz <matias.aleksandrowicz@gmail.com>
2024-09-11 01:38:47 -0300

    Added Nvim DAP and Nvim UI

--- init.lua
+++ init.lua
@@ -299,6 +299,34 @@ require("lazy").setup({
 			)
 		end,
 	},
+	{
+		"nvim-neotest/nvim-nio",
+	},
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"rcarriga/nvim-dap-ui",
+		},
+		config = function()
+			local dap = require("dap")
+			local dapui = require("dapui")
+
+			dap.listeners.before.attach.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				dapui.close()
+			end
+			vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, { desc = "Toggle Breakpoint" })
+			vim.keymap.set("n", "<Leader>dc", dap.continue, { desc = "Continue" })
+		end,
+	},
 	-- LSP Plugins
 	{
 		-- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
